cc47b68fda28befce7b20da354e47e72be390e69
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-12-27 21:55:52 -0400

    products componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { AddCategoryComponent } from './components/add-category/add-category.component';
 import { CategoryDetailsComponent } from './components/category-details/category-details.component';
 import { CategoriesListComponent } from './components/categories-list/categories-list.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddCategoryComponent,
     CategoryDetailsComponent,
-    CategoriesListComponent
+    CategoriesListComponent,
+    ProductsListComponent,
+    ProductDetailsComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule
