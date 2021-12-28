fc6dbd547d8aa5f05209302a23554524aa6e4a72
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-12-27 23:30:08 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # prueba-tas-2021
+
+# Use Angular version 13. Lenguaje Javascript
+# Abrir la terminal de comando, Para instalar con el comando npm install para ello debe tener instalado node y npm, se levanta con el comando ng serve, ubicarse en el navegador con la direccion que se genera en la terminal. 
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'https://my-json-server.typicode.com/TASNETWORK/Prueba-DJunior/categories';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  get(categori_id: any): Observable<any> {
+    return this.http.get(`${baseUrl}/${categori_id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
+  findByTitle(name: any): Observable<any> {
+    return this.http.get(`${baseUrl}?title=${name}`);
+  }
 }
