8333d5ad9d74ac0d628deffb5f2a2c92251939ff
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-12-27 21:54:36 -0400

    categorias componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { CategoryDetailsComponent } from './components/category-details/category-details.component';
+import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCategoryComponent,
+    CategoryDetailsComponent,
+    CategoriesListComponent
   ],
   imports: [
     BrowserModule
